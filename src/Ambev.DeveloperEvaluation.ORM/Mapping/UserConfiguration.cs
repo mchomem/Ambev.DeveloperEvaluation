5d417f85602c32825f9db0b67b02a4cb3aaddfea
Misael da Costa Homem <misael.homem@gmail.com>
2025-08-24 13:17:52 -0300

    included the CreateAt and UpdatedAt columns in the User entity mapping.

--- src/Ambev.DeveloperEvaluation.ORM/Mapping/UserConfiguration.cs
+++ src/Ambev.DeveloperEvaluation.ORM/Mapping/UserConfiguration.cs
@@ -17,13 +17,9 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
         builder.Property(u => u.Password).IsRequired().HasMaxLength(100);
         builder.Property(u => u.Email).IsRequired().HasMaxLength(100);
         builder.Property(u => u.Phone).HasMaxLength(20);
-
-        builder.Property(u => u.Status)
-            .HasConversion<string>()
-            .HasMaxLength(20);
-
-        builder.Property(u => u.Role)
-            .HasConversion<string>()
-            .HasMaxLength(20);
+        builder.Property(u => u.Status).HasConversion<string>().HasMaxLength(20);
+        builder.Property(u => u.Role).HasConversion<string>().HasMaxLength(20);
+        builder.Property(u => u.CreatedAt).IsRequired().HasColumnType("timestamptz");
+        builder.Property(u => u.UpdatedAt).IsRequired(false).HasColumnType("timestamptz");
     }
 }
